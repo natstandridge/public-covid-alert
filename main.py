7be9d2ef8d9030d9bc766740cdbcfdbe4f2e3e4c
Nat Standridge <standridgen@gmail.com>
2023-04-26 16:54:46 -0600

    Fixed incorrect character in --disable-gpu chrome option line.

--- main.py
+++ main.py
@@ -74,8 +74,8 @@ class Subscriber:
         chrome_options.add_argument('--disable-extension')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--headless')
-        chrome_options.add_argument("--window-size=1920,1080") ## MUST set window size for headless to work
-        chrome_options.add_argument('-–disable-gpu')
+        chrome_options.add_argument('--window-size=1920,1080') ## MUST set window size for headless to work
+        chrome_options.add_argument('--disable-gpu')
         chrome_options.add_experimental_option('useAutomationExtension', False)
         chrome_options.add_argument('--disable-infobars')
         chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
